f6547d18329d04ec735b1a3b4e6790a655d0ed5f
tuanlm173 <tuanlm.cca@gmail.com>
2022-03-09 16:49:21 +0700

    change facility id condition in dev

--- src/main/java/com/justanalytics/constant/CustomerBaseCondition.java
+++ src/main/java/com/justanalytics/constant/CustomerBaseCondition.java
@@ -22,7 +22,7 @@ public final class CustomerBaseCondition {
     public static final String CUSTOMER_CONTAINER_NAME = "api_customer";
 
     public static final String CUSTOMER_TYPE = "(c.terminal_customer_role IN (%s) AND IS_DEFINED(c.terminal_customer_role))";
-    public static final String FACILITY_ID = "(ARRAY_CONTAINS(c.facility_ids, %s, true))";
+    public static final String FACILITY_ID = "(c.facility_id IN (%s) AND IS_DEFINED(c.facility_id))"; //"(ARRAY_CONTAINS(c.facility_ids, %s, true))";
     public static final String CUSTOMER_NAME = "((c.terminal_account_name LIKE %s%s%s AND IS_DEFINED(c.terminal_account_name)) OR (c.account_name LIKE %s%s%s AND IS_DEFINED(c.account_name)))";
     public static final String TAX_ID = "((c.tax_id1 IN (%s) AND IS_DEFINED(c.tax_id1)) OR (c.tax_id2 IN (%s) AND IS_DEFINED(c.tax_id2)))";
 
